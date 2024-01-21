dbbd453602485bd59e0ebdb91be0aa3e58cef1bc
amuliawijaya23 <amuliawijaya@gmail.com>
2024-01-21 17:44:09 +0700

    prettier auto format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
-      <div>
-      </div>
+      <div></div>
     </main>
   );
 }
