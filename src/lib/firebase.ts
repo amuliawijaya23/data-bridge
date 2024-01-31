912daaf650b8d491d21e0aa3f7b330a1e5a2bd53
amuliawijaya23 <amuliawijaya@gmail.com>
2024-01-31 21:34:26 +0700

    adds getAuth to firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export { app, auth };
