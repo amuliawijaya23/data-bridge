cb3b40ad0e9500e1b62adc2812894519ec6528dc
amuliawijaya23 <amuliawijaya@gmail.com>
2024-02-05 16:46:23 +0700

    adds and implements authentication route group

---
+++ src/app/(authentication)/layout.tsx
@@ -0,0 +1,47 @@
+import {
+  Unstable_Grid2 as Grid,
+  Paper,
+  Box,
+  Typography,
+  Avatar,
+} from '@mui/material';
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Grid container component={Paper} sx={{ height: '100vh' }}>
+      <Grid
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: 'url("/background.jpg")',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: 'white',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            textAlign: 'center',
+          }}>
+          <Avatar src="" alt="logo" sx={{ width: 100, height: 100, mb: 2 }} />
+          <Typography component="h1" variant="h4">
+            SparkFlow
+          </Typography>
+          {children}
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
